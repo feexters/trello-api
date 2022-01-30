e289976e81a0cad533b5b10632304bec6c326d04
Danil Nepomnyashih <danil.nepomniashchikh@purrweb.com>
2022-01-30 23:00:15 +0600

    feature/user crud endpoints

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UsersRepository } from '../repositories/users.repository';
+import { UserUpdateDto } from '../dto/users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async getById(id: string) {
+    const user = await this.usersRepository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return user;
+  }
+
+  async updateUser(updateDto: UserUpdateDto, userId: string) {
+    const user = await this.getById(userId);
+    return this.usersRepository.save({ ...user, ...updateDto });
+  }
+
+  deleteUser(id: string) {
+    return this.usersRepository.delete(id);
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UserUpdateDto } from './dto/users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.getById(id);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() userUpdateDto: UserUpdateDto) {
+    return this.usersService.updateUser(userUpdateDto, id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+}
