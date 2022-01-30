99d35a34e412755fb34beced72d588ab56be6a16
Danil Nepomnyashih <danil.nepomniashchikh@purrweb.com>
2022-01-31 00:04:39 +0600

    feature/guard for users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UserUpdateDto } from './dto/users.dto';
+import { UserOwnerGuard } from './guards';
+import { JwtAuthGuard } from 'src/auth/guards';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  getUser(@Param('id') id: string) {
+  @Get(':userId')
+  getUser(@Param('userId') id: string) {
     return this.usersService.getById(id);
   }
 
-  @Patch(':id')
-  updateUser(@Param('id') id: string, @Body() userUpdateDto: UserUpdateDto) {
+  @UseGuards(JwtAuthGuard, UserOwnerGuard)
+  @Patch(':userId')
+  updateUser(
+    @Param('userId') id: string,
+    @Body() userUpdateDto: UserUpdateDto,
+  ) {
     return this.usersService.updateUser(userUpdateDto, id);
   }
 
-  @Delete(':id')
-  deleteUser(@Param('id') id: string) {
+  @UseGuards(JwtAuthGuard, UserOwnerGuard)
+  @Delete(':userId')
+  deleteUser(@Param('userId') id: string) {
     return this.usersService.deleteUser(id);
   }
 }
